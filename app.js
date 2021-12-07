ae66c947b0baaca787add298215f538d06a49a27
Madison Yarnall <maddycodes01@gmail.com>
2021-12-07 15:43:33 -0700

    buttons all work!

--- app.js
+++ app.js
@@ -64,9 +64,59 @@ draw()
 function missingPackage(){
   let packageIndex = Math.floor(Math.random() * packages.length)
   return packages[packageIndex] 
-
 }
 
 // missingPackage()
 console.log(missingPackage())
 
+
+function filterHeavy(){
+  let heavyPackage = packages.filter(package => package.heavy)
+  let template = ''
+  for (let i = 0; i < heavyPackage.length; i++){
+    const package = heavyPackage[i];
+    template += `<li>${package.heavy}</li>`
+  }
+  document.getElementById('packages'). innerHTML= template
+  console.log('helloworld')
+}
+
+function filterPriority(){
+  let priorityPackage = packages.filter(package => package.priority)
+  let template = ''
+  for(let i = 0; i < priorityPackage.length; i++){
+    const package = priorityPackage[i];
+    template += `<li>${package.priority}</li>`
+  }
+  document.getElementById('packages').innerHTML = template
+}
+
+function filterFragile(){
+  let fragilePackage = packages.filter(package => package.fragile)
+  let template = ''
+  for(let i = 0; i < fragilePackage.length; i++){
+    const package = fragilePackage[i];
+    template += `<li>${package.fragile}</li>`
+  }
+  document.getElementById('packages').innerHTML = template
+}
+
+function filterPerson(){
+  let personPackage = packages.filter(package => package.to)
+  let template = ''
+  for(let i = 0; i < personPackage.length; i++){
+    const package = personPackage[i];
+    template += `<li>${package.to}</li>`
+  }
+  document.getElementById('packages').innerHTML = template
+}
+
+function filterNumber(){
+  let numberPackage = packages.filter(package => package.trackingNumber)
+  let template = ''
+  for(let i = 0; i < numberPackage.length; i++){
+    const package = numberPackage[i];
+    template += `<li>${package.trackingNumber}</li>`
+  }
+  document.getElementById('packages').innerHTML = template
+}
