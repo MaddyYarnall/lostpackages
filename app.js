360dd8746d3898222324f92d6c638f9eb5655e95
Madison Yarnall <maddycodes01@gmail.com>
2021-12-07 14:02:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const packages = [
+  {
+heavy: true,
+priority: false,
+fragile: false,
+to: 'Harrington',
+trackingNumber: '1234kjs'
+},
+  {
+  heavy: false,
+  priority: true,
+  fragile: true,
+  to: 'Mark',
+  trackingNumber: '1325sdk'
+},
+{
+  heavy: true,
+  priority: false,
+  fragile: true,
+  to: 'Mick',
+  trackingNumber: 'jffd147'
+},
+{
+  heavy: false,
+  priority: false,
+  fragile: false,
+  to: 'Jake',
+  trackingNumber: 'acdc145'
+},
+{
+  heavy: true,
+  priority: true,
+  fragile: true,
+  to: 'Brittany',
+  trackingNumber: 'gsd481'
+},
+{
+  heavy: false,
+  priority: true,
+  fragile: false,
+  to: 'Jonsey',
+  trackingNumber: '8081baz'
+},
+{
+  heavy: true,
+  priority: false,
+  fragile: true,
+  to: 'Justin',
+  trackingNumber: 'suz2367'
+}
+]
+
